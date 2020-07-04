04cb21a8f371c3d2ff3eee753bfbdb1d5341906b
Michael DESKTOP <tan00060@algonquinlive.com>
2020-07-04 13:58:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+  let app = {
+
+      squares: document.querySelectorAll('.game-grid'),
+
+      yuki: document.createElement('IMG'),
+      brandon: document.createElement('IMG'),
+      currentPlayersTurn: true,
+
+      init: () => {
+          console.log("The next yuki meme game in the making.")
+          for (let i = 0; i < app.squares.length; i++) {
+              console.log(app.squares[i])
+              app.squares[i].addEventListener('click', app.pickingSquare)
+          }
+      },
+
+      pickingSquare: (ev) => {
+          ev.stopPropagation()
+
+          // Figure out which square is clicked
+          let pickedSquare = ev.target.classList
+          let appendHead = ev.target.id
+          console.log(ev.target.id)
+          console.log("This is the picked square " + pickedSquare)
+          if (pickedSquare[2] === 'x' || pickedSquare[2] === 'o') {
+              return
+          }
+
+          // Switch to player 2's turn when clicked
+          if (app.currentPlayersTurn) {
+              ev.target.classList.add("x")
+              // Sticking yukis head into the game
+            //   app.yukiHead(appendHead)
+              app.currentPlayersTurn = !app.currentPlayersTurn
+          } else {
+            //   app.brandonHead(appendHead)
+              // Sticking brandons head into the game
+              ev.target.classList.add("o")
+              app.currentPlayersTurn = !app.currentPlayersTurn
+          }
+
+      },
+
+      yukiHead: (target) => {
+          // Create img of Yuki head
+          let yukiHead = app.yuki
+          yukiHead.setAttribute("src", "img/YukiHead.png")
+          yukiHead.classList.add("yuki")
+
+          // Append Yuki Head to square clicked.
+          let addChoice = document.getElementById(target)
+          console.log(addChoice)
+          console.log(target)
+          addChoice.appendChild(yukiHead)
+
+          console.log("Yuki head is now in the game!")
+      },
+
+      brandonHead: (target) => {
+          // Create img of Brandon head
+          let brandonHead = app.brandon
+          brandonHead.setAttribute("src", "img/brandon.png")
+          brandonHead.classList.add("brandon")
+
+          // Append Brandon head to square clicked.
+          let addChoice = document.getElementById(target)
+          addChoice.appendChild(brandonHead)
+
+          console.log("Brandon head is now in the game!")
+      }
+  }
+
+  document.addEventListener('DOMContentLoaded', app.init);
